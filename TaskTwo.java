7e63882cc960f89004f32b50d33e3b3239f86ce3
Siddharth Naik <siddharthnaik7@live.com>
2021-01-07 13:21:36 -0800

    Made changes to TaskTwo.java

--- TaskTwo.java
+++ TaskTwo.java
@@ -75,54 +75,55 @@ public class TaskTwo {
 		
 		System.out.println("\n2. Operator based Task!");
 		
-		int selection;
+		char ch;
 		
 		do {
 			System.out.println();
-			int first, second, first1, second2;
+			int first = 0, second = 0, first1 = 0, second2 =0, result = 0;
 			System.out.println("Press \"1\" for Addition");
 			System.out.println("Press \"2\" for Subtraction");
 			System.out.println("Press \"3\" for Division");
 			System.out.println("Press \"4\" for Multiplication");
 			System.out.println("Press \"5\" for Average");
-			System.out.println("Press \"6\" to Exit");
+			System.out.println("Press \"s\" to Exit");
 			
 			System.out.println("Enter your selection: ");
-			selection = scanner.nextInt();
-			switch(selection) {
-				case 1:
-					System.out.println("Enter two number (Integers): ");
-					first = scanner.nextInt();
-					second = scanner.nextInt();
-					System.out.println("The addition of two numbers is: " + first + second);
+			ch = scanner.next().charAt(0);
+			
+			System.out.println("Enter two number (Integers): ");
+			if(ch=='1' || ch=='2' || ch=='3' || ch=='4') {
+				first = scanner.nextInt();
+				second = scanner.nextInt();
+			} else if (ch=='5') {
+				first1 = scanner.nextInt();
+				second2 = scanner.nextInt();
+			}
+			
+			switch(ch) {
+				case '1':
+					result = first+second;
 					break;
-				case 2:
-					System.out.println("Enter two number (Integers): ");
-					first = scanner.nextInt();
-					second = scanner.nextInt();
-					System.out.println("The subtraction of two numbers is: " + (first - second));
+				case '2':
+					result = first-second;
 					break;
-				case 3:
-					System.out.println("Enter two number (Integers): ");
-					first = scanner.nextInt();
-					second = scanner.nextInt();
-					System.out.println("The division of two numbers is: " + first/second);
+				case '3':
+					result = first/second;
 					break;
-				case 4:
-					System.out.println("Enter two number (Integers): ");
-					first = scanner.nextInt();
-					second = scanner.nextInt();
-					System.out.println("The multiplication of two numbers is: " + first*second);
+				case '4':
+					result = first*second;
 					break;
-				case 5:
-					System.out.println("Enter two number (Integers): ");
-					first1 = scanner.nextInt();
-					second2 = scanner.nextInt();
-					System.out.println("The average of two numbers is: " + (first1+second2)/2);
+				case '5':
+					result = (first1+second2)/2;
 					break;
 				
 			}
-		} while(selection<6);
+			if(result>0 && ch!='s') {
+				System.out.println("The Result is: " + result);
+			} else if(ch!='s') {
+				System.out.println("Option " + ch + " is returning a negative number!");
+			}
+			
+		} while(ch!='s');
 		
 		System.out.println("Operator Based Task Completed!");
 	}
